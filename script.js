9ac1bec5b9b0d6b6c47cc8d9c66956bad71a6797
keremkaya3 <keremky257@gmail.com>
2026-05-15 00:36:23 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -16,21 +16,36 @@ let guncelYil = 2026;
 let yas = guncelYil - dogumYili;
 console.log("Yaş:", yas);
 
-if (yas >= 0 && yas <= 6) {
+if (yas >= 0 && yas <= 6) 
+{
     console.log("Bebek / Okul öncesi");
-} else if (yas >= 7 && yas <= 14) {
+}
+ else if (yas >= 7 && yas <= 14)
+  {
     console.log("İlköğretim çağı");
-} else if (yas >= 15 && yas <= 18) {
+}
+ else if (yas >= 15 && yas <= 18) 
+{
     console.log("Lise çağı");
-} else if (yas >= 19 && yas <= 25) {
+} 
+else if (yas >= 19 && yas <= 25) 
+{
     console.log("Üniversite çağı");
-} else if (yas >= 26 && yas <= 40) {
+}
+ else if (yas >= 26 && yas <= 40) 
+{
     console.log("Genç yetişkin");
-} else if (yas >= 41 && yas <= 65) {
+} 
+else if (yas >= 41 && yas <= 65) 
+{
     console.log("Yetişkin");
-} else if (yas > 65) {
+} 
+else if (yas > 65) 
+{
     console.log("Emeklilik çağı");
-} else {
+} 
+else 
+{
     console.log("Geçersiz yaş değeri");
 }
 
@@ -43,28 +58,40 @@ let deger6 = "0";
 let deger7 = [];
 let deger8 = {};
 
-console.log("deger1:", Boolean(deger1));
-console.log("deger2:", Boolean(deger2));
-console.log("deger3:", Boolean(deger3));
-console.log("deger4:", Boolean(deger4));
-console.log("deger5:", Boolean(deger5));
-console.log("deger6:", Boolean(deger6));
-console.log("deger7:", Boolean(deger7));
-console.log("deger8:", Boolean(deger8));
+console.log("1.Değer:", Boolean(deger1));
+console.log("2.Değer:", Boolean(deger2));
+console.log("3.Değer:", Boolean(deger3));
+console.log("4.Değer:", Boolean(deger4));
+console.log("5.Değer:", Boolean(deger5));
+console.log("6.Değer:", Boolean(deger6));
+console.log("7.Değer:", Boolean(deger7));
+console.log("8.Değer:", Boolean(deger8));
 
 let ayNo = 5; 
 switch (ayNo) {
-    case 1: console.log("Ocak"); break;
-    case 2: console.log("Şubat"); break;
-    case 3: console.log("Mart"); break;
-    case 4: console.log("Nisan"); break;
-    case 5: console.log("Mayıs"); break;
-    case 6: console.log("Haziran"); break;
-    case 7: console.log("Temmuz"); break;
-    case 8: console.log("Ağustos"); break;
-    case 9: console.log("Eylül"); break;
-    case 10: console.log("Ekim"); break;
-    case 11: console.log("Kasım"); break;
-    case 12: console.log("Aralık"); break;
+    case 1: console.log("Ocak");
+    break;
+    case 2: console.log("Şubat"); 
+    break;
+    case 3: console.log("Mart"); 
+    break;
+    case 4: console.log("Nisan"); 
+    break;
+    case 5: console.log("Mayıs"); 
+    break;
+    case 6: console.log("Haziran"); 
+    break;
+    case 7: console.log("Temmuz"); 
+    break;
+    case 8: console.log("Ağustos"); 
+    break;
+    case 9: console.log("Eylül"); 
+    break;
+    case 10: console.log("Ekim"); 
+    break;
+    case 11: console.log("Kasım"); 
+    break;
+    case 12: console.log("Aralık"); 
+    break;
     default: console.log("Geçersiz ay numarası");
 }
